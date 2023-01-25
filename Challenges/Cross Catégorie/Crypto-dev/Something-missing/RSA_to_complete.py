dfeccd2e27e57808fd753944ce75385637902a4e
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2023-01-25 13:44:57 +0100

    Add files via upload

---
+++ Challenges/Cross Catégorie/Crypto-dev/Something-missing/RSA_to_complete.py
@@ -0,0 +1,60 @@
+import rsa, base64, sys
+
+def string_encode(string):
+    string = string.encode(encoding='UTF-8',errors='strict')
+    return base64.b64encode(string)
+
+def string_decode(string):
+    string = base64.b64decode(string)
+    return string.decode(encoding='UTF-8',errors='strict')
+
+def isBase64(s):
+    """
+    If the string is a valid base64 string, then the base64 decoded string will be the same as the
+    original string
+
+    :param s: The string to check
+    :return: The function isBase64() is returning a boolean value.
+    """
+
+def key_pair_generation():
+    (pubkey, prikey) = rsa.newkeys(1024, poolsize = 8)
+    return (pubkey, prikey)
+
+def encrypt(plain, pubkey):
+    cypher = rsa.encrypt(plain, pubkey)
+    return cypher
+
+def decrypt(cypher, prikey):
+    message = rsa.decrypt(cypher, prikey)
+    return message
+
+def handleBase64String(InputString, prikey):
+    decText = decrypt(InputString, prikey)
+    return string_decode(decText)
+
+def handleString(InputString, pubkey):
+    encText = string_encode(InputString)
+    return encrypt(encText, pubkey)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 RSA.pys MESSAGE\n\nUsing a very secret encryption key, the program will intelligently encrypt/decrypt your message!\n")
+        return
+    InputString = " ".join(sys.argv[1:])
+    InputString = InputString.replace('\n', "")
+
+    if isBase64(InputString):
+        print("Message already encrypted")
+    else :
+        pubkey, prikey = key_pair_generation()
+        encrypted = handleString(InputString, pubkey)
+        print("Encrypted text is : {}" .format(encrypted))
+        decrypted = handleBase64String(encrypted, prikey)
+        print("Decrypted text is : {}" .format(decrypted))
+
+if __name__ == "__main__":
+    main()
+
+
