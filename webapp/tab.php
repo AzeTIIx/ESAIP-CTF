90b809ad8c463a4275650df3628b649d0f67abb8
Alexandre Rouillé <112399735+arouille@users.noreply.github.com>
2023-04-15 17:03:57 +0200

    Update tab.php
    getflag() ajouté

--- webapp/tab.php
+++ webapp/tab.php
@@ -1,26 +1,16 @@
 <?php
 
-function getTopTenUsers() {
+require('config.php');
 
- 
-    // Informations d'identification
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'ctf');
-    
-    
-    // Connexion à la base de données MySQL 
-    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-    
-    // Vérifier la connexion
-    if($conn === false){
-        die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
-    }
 
+error_reporting(0);
+
+function getTopTenUsers($conn) {
+
+  
     $sql = "SELECT username, MAX(points) as points FROM tableaupoints GROUP BY username ORDER BY points DESC";
     $result = mysqli_query($conn, $sql);
-
+    
     if (mysqli_num_rows($result) > 0) {
         $users = array();
         while ($row = mysqli_fetch_assoc($result)) {
@@ -30,7 +20,6 @@ function getTopTenUsers() {
         echo "Aucun enregistrement trouvé.";
     }
 
-    mysqli_close($conn);
 
     // Sort the users by points in descending order
     usort($users, function($a, $b) {
@@ -40,7 +29,6 @@ function getTopTenUsers() {
     // Return the top 10 users
     return array_slice($users, 0, 10);
   }
-
 
 
 
@@ -69,3 +57,52 @@ echo "
 echo "</table>"
 ?>
 
+   
+function getflag($conn) {
+  // Vérifier si la connexion à la base de données est valide
+  if (!$conn) {
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+  }
+
+  $query = "SELECT `flag` FROM `challenges` WHERE `flag` = 'flag1'";
+  $result = mysqli_query($conn, $query);
+
+  // Vérifier si la requête s'est exécutée avec succès
+  if ($result) {
+    // Extraire la valeur de la colonne 'flag' du résultat de la requête
+    $row = mysqli_fetch_assoc($result);
+    $flagFromDB = $row['flag'];
+
+    // Vérifier si la clé 'flag' existe dans le tableau $_POST
+    if (isset($_POST['flag'])) {
+      // Récupérer la valeur soumise dans le champ de texte
+      $flag = $_POST['flag'];
+
+      // Vérifier si la valeur correspond à la valeur stockée dans la base de données
+      if ($flag === $flagFromDB) {
+        echo 'Challenge réussi !';
+
+      } else {
+        echo 'pas bon';
+        echo "<form method='post'>
+              <input type='input' class='box-input' name='flag' id='flag' placeholder='flag' required />
+              <input type='submit' class='box-input' name='envoyer' placeholder='envoyer'  />
+              </form>
+            ";
+      }
+    } else {
+      // La clé 'flag' n'est pas définie dans le tableau $_POST
+    }
+  } else {
+    // La requête a échoué
+   // echo 'ERREUR : Impossible d\'exécuter la requête. ' . mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
+}
+
+// Appel de la fonction getflag() en passant la variable $conn en paramètre
+getflag($conn);
+
+
+?>
