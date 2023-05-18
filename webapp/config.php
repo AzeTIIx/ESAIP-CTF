bfa82315f06169bb428d8874af302186b7692bc2
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2023-05-18 18:08:37 +0200

    Update config.php

--- webapp/config.php
+++ webapp/config.php
@@ -5,12 +5,12 @@ ini_set('display_errors', 'Off');
 // Informations d'identification
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_NAME', 'ctf');
- 
-// Connexion à la base de données MySQL 
+
+// Connexion à la base de données MySQL
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Vérifier la connexion
 if($conn === false){
     die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
